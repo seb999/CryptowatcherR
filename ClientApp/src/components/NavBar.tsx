7649a7ccb62fbb91b9317d21ac2697648164d193
Mikeldi <mikeldi@gmail.com>
2019-06-21 20:13:44 +0200

    Top Nav styles and body bk animation added

--- ClientApp/src/components/NavBar.tsx
+++ ClientApp/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { withRouter } from 'react-router-dom'
-import logo from '../images/LogoCryptowatcher-inv.png'
+import logo from '../images/cryptowatcher_LOGO_001_i.svg'
+import logoMobile from '../images/cryptowatcher_LOGO_001_i_mobile.svg'
 import './Css/NavBar.css';
 import MyNavLink from './Element/MyNavLink'
 
@@ -23,29 +24,36 @@ class NavBar extends React.Component<Props, State> {
   }
   render() {
     return (
-      <nav className="navbar transparent navbar-expand-lg navbar-dark">
-
-        <a className="navbar-brand" href="/"><img src={logo} className="logo" /></a>
-
-        <h3 className="brand" style={{color : "white"}}>Cryptowatcher 2.0</h3>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            {this.props.commands.map((link, i) => {
-              if (link.type === "NavLink") {
-                return (
-                  <MyNavLink key={i}
-                    path={link.path}
-                    text={link.text}
-                    isActive={link.isActive}
-                  />
-                );
-              }
-              return (<div key={i}></div>)
-            })}
-          </ul>
+      <nav className="navbar transparent navbar-expand-lg navbar-dark" id="mainNav">
+        <div className="container">
+          <a className="navbar-brand js-scroll-trigger" href="/">
+            <span className="d-lg-none">
+              <img src={logoMobile} className="logoMobile" />
+            </span>
+            <span className="d-none d-lg-block">
+              <img src={logo} className="logo" />
+            </span>
+          </a>
+          <button className="navbar-toggler navbar-toggler-right btn btn-primary-outline" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+      <i className="fas fa-bars" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav mr-auto">
+              {this.props.commands.map((link, i) => {
+                if (link.type === "NavLink") {
+                  return (
+                    <MyNavLink key={i}
+                      path={link.path}
+                      text={link.text}
+                      isActive={link.isActive}
+                    />
+                  );
+                }
+                return (<div key={i}></div>)
+              })}
+            </ul>
+          </div>
         </div>
       </nav>
     )
